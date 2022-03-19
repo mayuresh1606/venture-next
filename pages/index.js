085ac727ce5f209a6f83d753861cd15411b28a1a
mayuresh1606 <mayureshovhal16@gmail.com>
2022-03-20 00:31:59 +0530

    Fixed bugs about script and className

--- pages/index.js
+++ pages/index.js
@@ -93,96 +93,96 @@ export default function Home() {
   </Head>
   {/* <Script src='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p'></Script> */}
     <header>
-      <nav class="navbar">
-        <div class="nav-contents">
-          <div style={{"textAlign":"center"}} class="site-logo">
-            <h1 class="venture-logo">VENTURE WORLD</h1>
-            <h6 class="venture-logo-desc">Tour, Trek, Picnic, Pilgrimage</h6>
+      <nav className="navbar">
+        <div className="nav-contents">
+          <div style={{"textAlign":"center"}} className="site-logo">
+            <h1 className="venture-logo">VENTURE WORLD</h1>
+            <h6 className="venture-logo-desc">Tour, Trek, Picnic, Pilgrimage</h6>
           </div>
-          <div class="site-navigations">
-            <ul class="ul">
-              <li class="ul-background">
-                <a class="nav-link darkblue-color" aria-current="page" href="{% url 'index' %}">Home</a>
+          <div className="site-navigations">
+            <ul className="ul">
+              <li className="ul-background">
+                <a className="nav-link darkblue-color" aria-current="page" href="{% url 'index' %}">Home</a>
               </li>
-              <li class="ul-background">
-                <a class="nav-link darkblue-color" href="{% url 'tours' %}">Tours</a>
+              <li className="ul-background">
+                <a className="nav-link darkblue-color" href="{% url 'tours' %}">Tours</a>
               </li>
-              <li class="ul-background">
-                <a class="nav-link darkblue-color" href="{% url 'services' %}">Services</a>
+              <li className="ul-background">
+                <a className="nav-link darkblue-color" href="{% url 'services' %}">Services</a>
               </li>
-              <li class="ul-background">
-                <a class="nav-link darkblue-color" href="{% url 'contact-us' %}">Contact Us</a>
+              <li className="ul-background">
+                <a className="nav-link darkblue-color" href="{% url 'contact-us' %}">Contact Us</a>
               </li>
-              <li class="ul-background">
-                <a class="nav-link darkblue-color" href="{% url 'terms' %}">Terms</a>
+              <li className="ul-background">
+                <a className="nav-link darkblue-color" href="{% url 'terms' %}">Terms</a>
               </li>
             </ul>
           </div>
         </div>
       </nav>
     </header>
-    <div class="my-container">
-        <center><h5 class="venture-slogan">VENTURE WORLDS GLORIOUS 25 YEARS OF TOURING EXPERINCE</h5></center>
+    <div className="my-container">
+        <center><h5 className="venture-slogan">VENTURE WORLDS GLORIOUS 25 YEARS OF TOURING EXPERINCE</h5></center>
         
 
         <div id="slideshow-example" data-component="slideshow">
           <div role="list">
-            <div class="slide banner-img">
-              <img class="ban-img" src="/1banner.jpg" alt="err"/>
+            <div className="slide banner-img">
+              <img className="ban-img" src="/1banner.jpg" alt="err"/>
             </div>
-            <div class="slide banner-img">
-              <img class="ban-img" src="/3banner.jpg" alt="err"/>
+            <div className="slide banner-img">
+              <img className="ban-img" src="/3banner.jpg" alt="err"/>
             </div>
-            <div class="slide banner-img">
-              <img class="ban-img" src="/7banner.jpg" alt="err"/>
+            <div className="slide banner-img">
+              <img className="ban-img" src="/7banner.jpg" alt="err"/>
             </div>
           </div>
         </div>
-        <div class="imp-pack-top">
-          <div class="left-boxes">
-            <a class="tour-links" href="#tours-tables"><div class="box box-bg-yellow">
-              <p class="box-heading">Couple</p>
+        <div className="imp-pack-top">
+          <div className="left-boxes">
+            <a className="tour-links" href="#tours-tables"><div className="box box-bg-yellow">
+              <p className="box-heading">Couple</p>
             </div></a>
-            <a class="tour-links" href="#tours-tables"><div class="box box-bg-pink">
-              <p class="box-heading">HoneyMoon Special</p>
+            <a className="tour-links" href="#tours-tables"><div className="box box-bg-pink">
+              <p className="box-heading">HoneyMoon Special</p>
             </div></a>
-            <a class="tour-links" href="#tours-tables"><div class="box box-bg-green">
-              <p class="box-heading">Family</p>
+            <a className="tour-links" href="#tours-tables"><div className="box box-bg-green">
+              <p className="box-heading">Family</p>
             </div></a>
           </div>
-          <div class="right-box">
-            <table class="table table-bordered group-table border-secondary">
-              <thead class='thead-dark'>
+          <div className="right-box">
+            <table className="table table-bordered group-table border-secondary">
+              <thead className='thead-dark'>
                 <th colSpan={2}>
-                  <center class="darkblue-color font-weight"><a href="#tours-tables" class="darkblue-color tour-links">GROUP</a></center>
+                  <center className="darkblue-color font-weight"><a href="#tours-tables" className="darkblue-color tour-links">GROUP</a></center>
                 </th>
               </thead>
               <tbody>
                 <tr>
-                  <td><p class="box-heading"><a href="#tours-tables" class="tour-links maroon-color">STUDENTS</a></p></td>
-                  <td><p class="box-heading"><a href="#tours-tables" class="tour-links maroon-color">CORPORATE</a></p></td>
+                  <td><p className="box-heading"><a href="#tours-tables" className="tour-links maroon-color">STUDENTS</a></p></td>
+                  <td><p className="box-heading"><a href="#tours-tables" className="tour-links maroon-color">CORPORATE</a></p></td>
                 </tr>
                 <tr>
-                  <td><p class="box-heading"><a href="#tours-tables" class="tour-links maroon-color">SOCIETIES</a></p></td>
-                  <td><p class="box-heading"><a href="#tours-tables" class="tour-links maroon-color">LADIES EXCLUSIVE</a></p></td>
+                  <td><p className="box-heading"><a href="#tours-tables" className="tour-links maroon-color">SOCIETIES</a></p></td>
+                  <td><p className="box-heading"><a href="#tours-tables" className="tour-links maroon-color">LADIES EXCLUSIVE</a></p></td>
                 </tr>
                 <tr>
-                  <td><p class="box-heading"><a href="#tours-tables" class="tour-links maroon-color">SENIOR CITIZENS</a></p></td>
-                  <td><p class="box-heading"><a href="#tours-tables" class="tour-links maroon-color">BUDDIES</a></p></td>
+                  <td><p className="box-heading"><a href="#tours-tables" className="tour-links maroon-color">SENIOR CITIZENS</a></p></td>
+                  <td><p className="box-heading"><a href="#tours-tables" className="tour-links maroon-color">BUDDIES</a></p></td>
                 </tr>
               </tbody>
             </table>
           </div>
         </div>
-        <div class="my-tables">
-          <div class="imp-packs">
+        <div className="my-tables">
+          <div className="imp-packs">
             <section id="tours-tables">
-              <h3 class="imp-heading"></h3>
+              <h3 className="imp-heading"></h3>
               <hr />
-              <table class="table table-striped table-light table-responsive">
+              <table className="table table-striped table-light table-responsive">
                 <thead>
                   <tr>
-                    <th class="table-bordered" scope="col">Destination</th>
+                    <th className="table-bordered" scope="col">Destination</th>
                     <th scope="col">Starting @</th>
                     <th scope="col">Details</th>
                   </tr>
@@ -190,22 +190,22 @@ export default function Home() {
                 <tbody>
                   <tr>
                     <td>
-                      <span class="font-weight blue-color">FABULOUS HIMACHAL –</span> <br/>
+                      <span className="font-weight blue-color">FABULOUS HIMACHAL –</span> <br/>
                       SHIMLA / MANALI / DHARAMSHALA / DALHOUSIE /
                       PALAMPUR / PRAGPUR / CHANDIGARH (U.T) / AMRITSAR (PB)
                     </td>
-                    <td class="font-weight">Rs.17,750/- per person</td>
+                    <td className="font-weight">Rs.17,750/- per person</td>
                     <td><a href="/himachal">Click for more
                       details</a></td>
                     </tr>
                   <tr>
                     <td>
-                      <span class="font-weight purple-color">MAJESTIC KARNATAKA –</span> <br/>
+                      <span className="font-weight purple-color">MAJESTIC KARNATAKA –</span> <br/>
                       BENGALURU / MYSURU / OOTY (TN) / HASSAN / COORG /
                       CHIKMAGALUR / CHITRADURGA / HAMPI / BADAMI /
                       MANGALURU / GOA
                     </td>
-                    <td class="font-weight">Rs.16,750/- per person</td>
+                    <td className="font-weight">Rs.16,750/- per person</td>
                     <td><a href="karnataka">Click for more
                       details</a></td>
                     </tr>
@@ -214,26 +214,26 @@ export default function Home() {
             </section>
             
           </div>
-          <div class="aside-tables">
+          <div className="aside-tables">
             <a style={{"textDecoration": "none"}} href="{% url 'educational_tours' %}">
-              <div class="box-2">
-                <p class="box-heading">Educational Tours</p>
+              <div className="box-2">
+                <p className="box-heading">Educational Tours</p>
               </div>
             </a>
             <a style={{"textDecoration": "none"}} href="/adventure">
-              <div class="box-2 box-bg-darkgreen">
-                <p class="box-heading">Adventure Camp</p>
+              <div className="box-2 box-bg-darkgreen">
+                <p className="box-heading">Adventure Camp</p>
               </div>
             </a>
             <a style={{"textDecoration": "none"}} href="{% url 'tour_details' 'pilgrimage' %}">
-              <div class="box-2 box-bg-red-orange">
-                <p class="box-heading">Pilgrimage</p>
+              <div className="box-2 box-bg-red-orange">
+                <p className="box-heading">Pilgrimage</p>
               </div>
             </a>
           </div>
         </div>
         <hr />
-        <p class="venture-description blue-color">Welcome to the Venture World to enjoy great getaways. Whilst assuring you the
+        <p className="venture-description blue-color">Welcome to the Venture World to enjoy great getaways. Whilst assuring you the
           lifetime holiday experience & take you
           to
           places dreams are made of. We aim to provide you a complete holiday package.
@@ -248,13 +248,13 @@ export default function Home() {
           colors spread all over the world.</p>
     </div>
     <footer>
-      <div class="footer-details">
-        <div class="copyright">
-          <span>Copyright <span class="current-year">2012</span> | Venture World Tours & Travels.</span>
+      <div className="footer-details">
+        <div className="copyright">
+          <span>Copyright <span className="current-year">2012</span> | Venture World Tours & Travels.</span>
         </div>
-        <div class="dev-details">
-          <span class="dev">Developed by - Mayuresh Ovhal</span><br />
-          <span class="dev">mayureshovhal16@gmail.com</span>
+        <div className="dev-details">
+          <span className="dev">Developed by - Mayuresh Ovhal</span><br />
+          <span className="dev">mayureshovhal16@gmail.com</span>
         </div>
       </div>
     </footer>
--- pages/karnataka.js
+++ pages/karnataka.js
@@ -7367,16 +7367,16 @@ export default function Karnataka(){
     {/* {/* <!-- Optional JavaScript; choose one of the two! --> */}
 
     {/* <!-- Option 1: Bootstrap Bundle with Popper --> */}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    {/* <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossorigin="anonymous">
     </script>
     {/* <!-- JavaScript Bundle with Popper --> */}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    {/* <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossorigin="anonymous"></script>
     <script src="/tours/static/tours/main.js">
-    </script> 
+    </script>   */}
 
     {/* <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
