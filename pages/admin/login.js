1b8b31a974f4b33edae35fc5109faaf53209983d
mayuresh1606 <mayureshovhal16@gmail.com>
2022-04-13 17:55:51 +0530

    Fixed login page

--- pages/admin/login.js
+++ pages/admin/login.js
@@ -1,3 +1,14 @@
 export default function Login(){
-    return;
+    return <>
+        <div className="my-container">
+            <form className="tours-form">
+                <div>
+                    <input type="text" placeholder="Enter your user name" />
+                </div>
+                <div>
+                    <input type="text" placeholder="Enter your user password" />
+                </div>
+            </form>
+        </div>
+    </>;
 }
