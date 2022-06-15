5420bf89b317ae7c3b1a3b7fe5ad7928d7de2ce5
mayuresh1606 <mayureshovhal16@gmail.com>
2022-06-15 11:47:02 +0530

    Updated map

--- pages/contactUs.js
+++ pages/contactUs.js
@@ -19,10 +19,7 @@ export default function Contact(){
             </div>
             <div className="address-location">
                 <center>
-                <iframe className="map-venture"
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3767.7767842473795!2d72.97602834621483!3d19.204949392817387!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b9396dd09805%3A0xe7e9ce84f716aebd!2sVenture%20World%2C%20Swagath%20Travels!5e0!3m2!1sen!2sin!4v1627115862443!5m2!1sen!2sin"
-                    width="40vw" height="40vh" style={{border:0}} allowFullScreen="" loading="lazy">
-                </iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3767.951769998783!2d72.98059441490297!3d19.197308787018414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xaddfddeb7ab1bc05!2zMTnCsDExJzUwLjMiTiA3MsKwNTgnNTguMCJF!5e0!3m2!1sen!2sin!4v1655273016000!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                 </center>
             </div>
         </div>
